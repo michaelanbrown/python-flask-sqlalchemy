6d8960f4765fd37f656e696ac55730a3ac4b6ed0
michaelanbrown <m_brownbrown@yahoo.com>
2024-12-30 12:12:59 -0800

    set SQLALCHEMY_TRACK_MODIFICATIONS to false

--- server/app.py
+++ server/app.py
@@ -8,6 +8,7 @@ from models import db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 migrate = Migrate(app, db)
 
